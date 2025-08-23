78d3a474dd6dfed856b140d4723a25ad4db57f22
takoyaki333 <mie.kurahashi@gmail.com>
2025-08-24 04:38:50 +0900

    feat: 実績確認ページを実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,24 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import "chartkick"
+import "chart.js"
+
+Chart.register({
+  id: "centerText",
+  beforeDraw(chart) {
+    if (chart.config.options.plugins.centerText) {
+      const { ctx, chartArea: { width, height } } = chart;
+      const text = chart.config.options.plugins.centerText.text;
+
+      ctx.save();
+      ctx.font = "bold 14px sans-serif";
+      ctx.fillStyle = "#f97316"; // オレンジ色で表示
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillText(text, width / 2, height / 2);
+    }
+  }
+});
+
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,10 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+
+pin "chartkick", to: "chartkick.js"
+pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.js"
+pin "chartjs-plugin-datalabels", to: "https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.2.0/dist/chartjs-plugin-datalabels.js"
+pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.4
+
 pin_all_from "app/javascript/controllers", under: "controllers"
